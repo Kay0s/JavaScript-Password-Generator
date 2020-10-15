7a942ab2dce1cb689d0bbc5ab9747440bcf79ab5
Kay0s <hamilton.kristina@gmail.com>
2020-10-15 14:44:42 -0500

    updated js for prompt and confirmation boxes

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,6 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
-  //set password length 8 - 128 characters
-  let pwLength = document.getElementById("prompt").value;
 
   // password variables -  uppercase, lowercase, numerical and special characters
   let upperCase  = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -13,24 +11,68 @@ let generateBtn = document.querySelector("#generate");
   
 
 //create for loop to choose password characters if upperCase, lowerCase, inclNumeric, inclSpecial are chosen
-for (let i = 0; i <= pwLength; i++){
-  let password = upperCase.concat(lowerCase, inclNumeric, inclSpecial);
-}
+// for (let i = 0; i <= pwLength; i++){
+//   let password = upperCase.concat(lowerCase, inclNumeric, inclSpecial);
+// }
 
-//create for loop to choose password characters if upperCase, lowerCase, and inclNumeric 
-for (let i = 0; i <= pwLength; i++){
-  let password = upperCase.concat(lowerCase, inclNumeric);
-}
+// //create for loop to choose password characters if upperCase, lowerCase, and inclNumeric 
+// for (let i = 0; i <= pwLength; i++){
+//   let password = upperCase.concat(lowerCase, inclNumeric);
+// }
 
-//create for loop to choose password characters if upperCase and lowerCase are chosen
-for (let i = 0; i <= pwLength; i++){
- let password = upperCase.concat(lowerCase);
-}
+// //create for loop to choose password characters if upperCase and lowerCase are chosen
+// for (let i = 0; i <= pwLength; i++){
+//  let password = upperCase.concat(lowerCase);
+// }
 
-//create for loop to choose password characters if upperCase is chosen
-for (let i = 0; i <= pwLength; i++){
-  let password = upperCase;
+// //create for loop to choose password characters if upperCase is chosen
+// for (let i = 0; i <= pwLength; i++){
+//   let password = upperCase;
+//  } 
+
+//generate password function
+function generatePassword () {
+  //set password length 8 - 128 characters
+  let pwLength = prompt("Choose a password length of at least 8 characters, but not more than 128");
+  if (pwLength === null || pwLength)("Please enter desired character length for generated password");{
+    txt = "user cancelled the prompt.";
+    } if (pwLength => 8|| pwLength)("Please enter desired character length of at least 8 characters");{
+    txt = "user enter numb less than 8.";
+     } if (pwLength => 128|| pwLength)("Please enter desired character length of a maximum of 128 characters");{
+    txt = "user enter numb greater than 128.";
+  }
+    // } if else document.getElementbyID("pwLength").innerHTML = "Your password will be" + pwLength + "characters long.";
+
+  //confirm if user wants lowercase characters
+  let lowerCase = confirm("Use lowercase characters?");
+  if (lowerCase === null || lowerCase("Lowercase characters won't be used");{
+    txt = "user declined lowerCase";
+  }
+
+  //confirm if user wants uppercase characters
+  let upperCase = confirm("Use uppercase characters?");
+  if (upperCase === null || upperCase("Uppercase characters won't be used");{
+    txt = "user declined lowerCase";
+  }
+
+
+ //confirm if user wants numeric characters
+ let inclNumeric = confirm("Use numeric characters?");
+ if (inclNumeric === null || inclNumeric("Numeric characters won't be used");{
+   txt = "user declined inclNumeric";
  }
+
+
+
+//confirm if user wants special characters
+let inclSpecial = confirm("Use special characters?");
+if (inclSpecial === null || inclSpecial("Special characters won't be used");{
+  txt = "user declined inclSpecial";
+}
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
