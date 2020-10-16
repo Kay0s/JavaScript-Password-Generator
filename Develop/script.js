340781ff8e2cb61e3e39d633f599139478137248
Kay0s <hamilton.kristina@gmail.com>
2020-10-16 12:31:45 -0500

    Updated confirm and alert code.

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,84 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
+ 
+//generate password function
+// function generatePassword () {
+  //set password length 8 - 128 characters
+  let pwLength = prompt("Choose a password length of at least 8 characters, but not more than 128");
+  if (pwLength === null){
+    alert("Please enter desired character length for generated password");
+    } else if (pwLength < 8){
+      alert("Please enter desired character length of at least 8 characters");
+     } else if (pwLength > 128){
+     alert("Please enter desired character length of a maximum of 128 characters");
+     }else if (isNaN){
+     prompt("Please enter a number from 8 to 128 for desired character length");
+     }
+ 
+
+let choiceArray = [];
+
+  //confirm if user wants lowercase characters
+  let lowerCaseConfirm = confirm("Use lowercase characters?");
+  if (lowerCaseConfirm) {
+  choiceArray.push("lowerCaseChosen");
+  }
+  //confirm if user wants uppercase characters
+  let upperCaseConfirm = confirm("Use uppercase characters?");
+  if (upperCaseConfirm) {
+    choiceArray.push("upperCaseChosen");
+    }
 
 
-  // password variables -  uppercase, lowercase, numerical and special characters
-  let upperCase  = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l","m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  let inclNumeric = [0,1,2,3,0,4,5,6,7,8,9];
-  //info from OWASP Foundation
-  let inclSpecial = ["!", "\s", "#", "$", "%", "&", "/'", "(", ")", "*", "+", ",", "-", ".", "/:", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
-  
+ //confirm if user wants numeric characters
+ let inclNumericConfirm = confirm("Use numeric characters?");
+ if (inclNumericConfirm) {
+  choiceArray.push("inclNumericChosen");
+  }
+
+
+//confirm if user wants special characters
+let inclSpecialConfirm = confirm("Use special characters?");
+if (inclSpecialConfirm) {
+  choiceArray.push("inclSpecialChosen");
+  }
+
+// generate password
+function generatePassword() {
+  let password = "";
+
+  let option = choiceArray[Math.floor(Math.random()*choiceArray.length)];
+  if (option === "lowerCaseChosen"){
+  password += lowerCaseArray[Math.floor(Math.random()*26)];
+  } if (option === "upperCaseChosen"){
+    password += upperCaseArray[Math.floor(Math.random()*26)];
+  }
+}
+
+// for x=0 if x less than pwLength x++
+
+//return password in generate password
+
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+ // password variables -  uppercase, lowercase, numerical and special characters
+ let upperCaseArray  = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+ let lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l","m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+ let inclNumericArray = [0,1,2,3,0,4,5,6,7,8,9];
+ //info from OWASP Foundation
+ let inclSpecialArray = ["!", "\s", "#", "$", "%", "&", "/'", "(", ")", "*", "+", ",", "-", ".", "/:", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+ 
 
 //create for loop to choose password characters if upperCase, lowerCase, inclNumeric, inclSpecial are chosen
 // for (let i = 0; i <= pwLength; i++){
@@ -30,58 +100,4 @@ let generateBtn = document.querySelector("#generate");
 //   let password = upperCase;
 //  } 
 
-//generate password function
-function generatePassword () {
-  //set password length 8 - 128 characters
-  let pwLength = prompt("Choose a password length of at least 8 characters, but not more than 128");
-  if (pwLength === null || pwLength)("Please enter desired character length for generated password");{
-    txt = "user cancelled the prompt.";
-    } if (pwLength => 8|| pwLength)("Please enter desired character length of at least 8 characters");{
-    txt = "user enter numb less than 8.";
-     } if (pwLength => 128|| pwLength)("Please enter desired character length of a maximum of 128 characters");{
-    txt = "user enter numb greater than 128.";
-  }
-    // } if else document.getElementbyID("pwLength").innerHTML = "Your password will be" + pwLength + "characters long.";
 
-  //confirm if user wants lowercase characters
-  let lowerCase = confirm("Use lowercase characters?");
-  if (lowerCase === null || lowerCase("Lowercase characters won't be used");{
-    txt = "user declined lowerCase";
-  }
-
-  //confirm if user wants uppercase characters
-  let upperCase = confirm("Use uppercase characters?");
-  if (upperCase === null || upperCase("Uppercase characters won't be used");{
-    txt = "user declined lowerCase";
-  }
-
-
- //confirm if user wants numeric characters
- let inclNumeric = confirm("Use numeric characters?");
- if (inclNumeric === null || inclNumeric("Numeric characters won't be used");{
-   txt = "user declined inclNumeric";
- }
-
-
-
-//confirm if user wants special characters
-let inclSpecial = confirm("Use special characters?");
-if (inclSpecial === null || inclSpecial("Special characters won't be used");{
-  txt = "user declined inclSpecial";
-}
-
-
-
-
-
-// Write password to the #password input
-function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
